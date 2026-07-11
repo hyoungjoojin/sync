f7734e7990fda53e82e3b13b956d981ba8608358
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2026-07-11 09:43:40 +0900

    modify onboarding stage

--- apps/server/src/main/java/com/skkil/sync/config/SecurityConfig.java
+++ apps/server/src/main/java/com/skkil/sync/config/SecurityConfig.java
@@ -77,6 +77,7 @@ public class SecurityConfig {
                     .requestMatchers(
                         "/users/**",
                         "/profiles/me",
+                        "/profiles/me/**",
                         "/media/**",
                         "/providers/my/**",
                         "/preferences/**")
